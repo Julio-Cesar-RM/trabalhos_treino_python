e3e1fdca1327cd890d38cae19273ad3736179e97
Julio-Cesar-RM <163890118+Julio-Cesar-RM@users.noreply.github.com>
2024-08-08 22:33:12 -0300

    recursos finais

---
+++ arquivos/recursos_finais.py
@@ -0,0 +1,39 @@
+#trocar valores entre duas variáveis
+
+# var1 = 12
+
+# var2 = 31
+
+# var2, var1 = var1, var2
+
+# print(var1, var2)
+
+#=========================
+
+#operador ternario
+
+# var1 = 32
+# var2 = 12
+
+# menor = var1 if var1 < var2 else var2
+
+# print(menor)
+
+#========================
+#generator
+
+# valores = [121,142,65,1,123,653]
+# quadrados = list([item**2 for item in valores])
+# print(quadrados)
+
+#=============================
+
+#enumarate
+
+# bebidas = ['cafe', 'cha', 'agua', 'suco', 'refrigerante']
+
+# for i, j in enumerate(bebidas):
+#     print(f'índice: {i} // item: {j}')
+
+#==================================
+
